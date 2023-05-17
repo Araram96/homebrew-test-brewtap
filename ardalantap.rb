64cd9f78ed7562523491eae2fb8e9e2101de68c3
ardalan <ardalan@dataroots.io>
2023-05-17 14:28:38 +0200

    rb file add

---
+++ ardalantap.rb
@@ -0,0 +1,17 @@
+
+class Ardalantap < Formula
+  desc "this is an ardalan test"
+  homepage "https://github.com/Araram96/test-brewtap"
+  url "https://github.com/Araram96/test-brewtap/archive/refs/tags/v0.2.0.tar.gz"
+  sha256 "baed683334805c32178038242368ccefcaccfcf8ad8e0f658241f1a499b3fe88"
+  license "MIT"
+
+  # depends_on "cmake" => :build
+
+  def install
+    
+    bin.install 'test_script.py' -> 'test_script'
+
+  end
+
+end
