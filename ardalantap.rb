9f208cbe6740aa669f13c2b52e3cf565c07cafda
ardalan <ardalan@dataroots.io>
2023-05-22 14:02:27 +0200

    hey

--- ardalantap.rb
+++ ardalantap.rb
@@ -8,41 +8,36 @@ class Ardalantap < Formula
   depends_on "cmake" => :build
   depends_on "python@3.9"
   
-  def install 
+ 
+    # Create a wrapper script that runs the Python script
+  def install
     # Install the Python script
     libexec.install "test_script.py"
+
     # Make the Python script executable
     chmod "+x", libexec/"test_script.py"
 
     # Create a wrapper script that runs the Python script
-    def install
-      # Install the Python script
-      libexec.install "test_script.py"
-  
-      # Make the Python script executable
-      chmod "+x", libexec/"test_script.py"
-  
-      # Create a wrapper script that runs the Python script
-      (bin/"ardalantap").write <<~EOS
-        #!/bin/bash
-        exec "#{Formula["python@3.9"].opt_bin}/python3" "#{libexec}/test_script.py" "$@"
-      EOS
-  
-      # Make the wrapper script executable
-      chmod "+x", bin/"ardalantap"
-    end
-  
-    def caveats
-      <<~EOS
-        The Python script is installed in the following location:
-          #{opt_libexec}/test_script.py
-  
-        You can run it using the "ardalantap" command.
-      EOS
-    end
-  
-    test do
-      # Add test code here to verify the installation
-      system "#{bin}/ardalantap", "--version"
-    end
+    (bin/"ardalantap").write <<~EOS
+      #!/bin/bash
+      exec "#{Formula["python@3.9"].opt_bin}/python3" "#{libexec}/test_script.py" "$@"
+    EOS
+
+    # Make the wrapper script executable
+    chmod "+x", bin/"ardalantap"
   end
+
+  def caveats
+    <<~EOS
+      The Python script is installed in the following location:
+        #{opt_libexec}/test_script.py
+
+      You can run it using the "ardalantap" command.
+    EOS
+  end
+
+  test do
+    # Add test code here to verify the installation
+    system "#{bin}/ardalantap", "--version"
+  end
+end
