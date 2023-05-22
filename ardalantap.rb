697e391254cbccebaa8f30a2fffd7a377fc5eaca
ardalan <ardalan@dataroots.io>
2023-05-22 13:48:13 +0200

    v new

--- ardalantap.rb
+++ ardalantap.rb
@@ -1,11 +1,11 @@
 class Ardalantap < Formula
   desc "Test3"
-  homepage "Araram96/test-brewtap"
-  url "https://github.com/Araram96/homebrew-test-brewtap/archive/refs/tags/v0.2.2.tar.gz"
-  sha256 "11e45f4afb6ce1e201910e4e618a08395f38190527c29c28746f1124f3c8f3b5"
+  homepage "https://github.com/Araram96/test-brewtap"
+  url "https://github.com/Araram96/test-brewtap/archive/refs/tags/v0.2.4.tar.gz"
+  sha256 "6a86f1e8ffaa66b7ae9c4d9303b30bd34a96c804886252b535eb04d9e333eb8a"
   license "MIT"
   
-  depends_on "python@3.10.9"
+  depends_on "python@3.10"
   depends_on "cmake" => :build
 
   def install
